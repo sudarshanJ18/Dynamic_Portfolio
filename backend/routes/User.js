2c4c4e88a62a31fa80057ce1332dd858abe70b21
sudarshanJ18 <jsudarshanreddy2003@gmail.com>
2024-12-03 00:28:35 +0530

    2

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -14,16 +14,13 @@ import {
   deleteProject,
 } from "../controller/User.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+
 export const userRouter = express.Router();
 
 userRouter.route("/login").post(login);
-
 userRouter.route("/logout").get(logout);
-
 userRouter.route("/user").get(getUser);
-
 userRouter.route("/me").get(isAuthenticated, myProfile);
-
 userRouter.route("/admin/update").put(isAuthenticated, updateUser);
 
 userRouter.route("/admin/timeline/add").post(isAuthenticated, addTimeline);
